023353f9b03fa6ffda25ac000e5a157cd1f7d3cd
pengyu <yypeng918@163.com>
2017-08-31 00:35:34 +0800

    增加类数据库操作类文件，修改类配置文件

---
+++ core/Db.php
@@ -0,0 +1,25 @@
+<?php
+//数据库操作类
+class Db {
+    private $DB=null;  //数据库实例化
+    function __construct(){
+        if($this->DB==null){
+            $dbms=cfg("db_type");
+            $host=cfg("db_host");
+            $dbName=cfg("db_name");
+            $dsn="$dbms:host=$host;dbname=$dbName";
+            try {
+                $this->DB = new PDO($dsn, cfg("db_user"), cfg("db_pwd"));
+            } catch (PDOException $e) {
+                die('Connection failed: ' . $e->getMessage());
+            }
+        }
+    }
+
+    function find(){
+        
+    }
+
+
+
+}
