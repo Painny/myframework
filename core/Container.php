d03eb4091d7646b03ce33b808d1c7538996db070
pengyu <917103429@qq.com>
2019-08-28 17:58:48 +0800

    容器绑定实现

--- core/Container.php
+++ core/Container.php
@@ -28,9 +28,35 @@ class Container
         return self::$instance;
     }
 
-    public function bind($abstract,$concrete)
+    public static function bind($abstract,$concrete)
     {
+        return self::$instance->bindTo($abstract,$concrete);
+    }
 
+    public function bindTo($abstract,$concrete)
+    {
+        if (is_object($concrete)) {
+            $this->bindInstance[$abstract]=$concrete;
+        } else {
+            $this->bindList[$abstract]=$concrete;
+        }
+        return;
+    }
+
+    public static function get($abstract)
+    {
+        return self::$instance->build($abstract);
+    }
+
+    public function build($abstract,$param)
+    {
+        if (isset($this->bindInstance[$abstract])) {
+            return $this->bindInstance[$abstract];
+        } else if ($this->bindList[$abstract] instanceof \Closure) {
+            //todo 闭包 判断是否有参数需要生成
+        } else {
+            //todo 类名 判断是否存在，并且是否有参数需要生成
+        }
     }
 
 }
