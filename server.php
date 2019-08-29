6d391ffd32d8b35b8b324d87fcdbfea4f11c3ff7
pengyu <917103429@qq.com>
2019-08-29 16:58:40 +0800

    使用pengyu/server当服务器测试

---
+++ server.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/4/26
+ * Time: 14:20
+ */
+
+require_once "vendor/autoload.php";
+
+$server=new Pengyu\Server\Worker\Server("http://0.0.0.0:8080");
+
+$server->on("workerStart",function (){
+    $database=new \Medoo\Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'blog',
+        'server'        => 'localhost',
+        'username'      => 'root',
+        'password'      => 'root'
+    ]);
+
+    \Core\Container::bind("Medoo\Medoo",$database);
+
+    \Core\Router::clear();
+    \Core\Router::load("./route/web.php");
+});
+
+$server->on("request",function ($data,$response){
+    $url=explode("?",$data["server"]["REQUEST_URI"],2);
+    try {
+        $result=\Core\Router::runMatch($url[0],$data["server"]["REQUEST_METHOD"]);
+        $response->end($result);
+    } catch (Exception $exception) {
+        //todo 异常分类判断
+        if ($exception->getMessage() == "404 not found") {
+            $response->status(404);
+        }
+        $response->end($exception->getMessage());
+    }
+});
+
+$server->run();
