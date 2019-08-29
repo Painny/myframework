e0b03fb68ac50b455dabe3ab4c55a8264da071e4
pengyu <917103429@qq.com>
2019-08-29 15:30:06 +0800

    控制器基类

---
+++ app/Controller/Base.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pengyu
+ * Date: 2019/8/29
+ * Time: 15:01
+ */
+
+namespace App\Controller;
+
+use Medoo\Medoo;
+
+class Base
+{
+    protected $db;
+
+    public function __construct(Medoo $db)
+    {
+        $this->db=$db;
+    }
+
+    public function end($data,$type="json")
+    {
+        if ($type == "json") {
+            header("Content-Type: application/json");
+        }
+
+        if (!is_string($data)) {
+            $data=json_encode($data);
+        }
+
+        echo $data;
+    }
+}
