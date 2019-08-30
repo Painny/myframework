691eef0092dbeb2a3143d597690265b9c8fcdef5
pengyu <917103429@qq.com>
2019-08-30 16:27:20 +0800

    待完成

--- core/init.php
+++ core/init.php
@@ -8,13 +8,17 @@
 
 require_once "../vendor/autoload.php";
 
+\Core\Config::loadDir("../config");
+
 //初始化数据库
 $database=new \Medoo\Medoo([
-    'database_type' => 'mysql',
-    'database_name' => 'blog',
-    'server'        => 'localhost',
-    'username'      => 'root',
-    'password'      => 'root'
+    'database_type' =>  config("db.driver"),
+    'database_name' =>  config("db.db"),
+    'server'        =>  config("db.host"),
+    'port'          =>  config("db.port"),
+    'username'      =>  config("db.user"),
+    'password'      =>  config("db.password"),
+    'prefix'        =>  config("db.prefix")
 ]);
 
 \Core\Container::bind("Medoo\Medoo",$database);
@@ -22,6 +26,21 @@ $database=new \Medoo\Medoo([
 \Core\Router::clear();
 \Core\Router::load("../route/web.php");
 
-//todo 初始化session cache等，设置时区
+date_default_timezone_set(config("app.timezone"));
 
+//todo 初始化session cache等
 
+//todo 获取url和method
+$url="index/say/hello";
+$method="GET";
+try {
+    $data=\Core\Router::runMatch($url,$method);
+} catch (Throwable $throwable) {
+    http_response_code(500);
+    echo $throwable->getMessage();
+    return;
+}
+
+echo $data;
+
+//todo 收尾工作
