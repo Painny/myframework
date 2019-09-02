5bcd3e34723b383beeaa81b3ced2895a1f0375c5
pengyu <917103429@qq.com>
2019-09-02 15:02:43 +0800

    从$_SERVER中获取uri和method

--- core/init.php
+++ core/init.php
@@ -10,18 +10,17 @@ require_once "../vendor/autoload.php";
 
 \Core\Config::loadDir("../config");
 
-//初始化数据库
-$database=new \Medoo\Medoo([
-    'database_type' =>  config("db.driver"),
-    'database_name' =>  config("db.db"),
-    'server'        =>  config("db.host"),
-    'port'          =>  config("db.port"),
-    'username'      =>  config("db.user"),
-    'password'      =>  config("db.password"),
-    'prefix'        =>  config("db.prefix")
-]);
-
-\Core\Container::bind("Medoo\Medoo",$database);
+\Core\Container::bind("DB",function (){
+    return new \Medoo\Medoo([
+        'database_type' =>  config("db.driver"),
+        'database_name' =>  config("db.db"),
+        'server'        =>  config("db.host"),
+        'port'          =>  config("db.port"),
+        'username'      =>  config("db.user"),
+        'password'      =>  config("db.password"),
+        'prefix'        =>  config("db.prefix")
+    ]);
+});
 
 \Core\Router::clear();
 \Core\Router::load("../route/web.php");
@@ -30,11 +29,11 @@ date_default_timezone_set(config("app.timezone"));
 
 //todo 初始化session cache等
 
-//todo 获取url和method
-$url="index/say/hello";
-$method="GET";
+//获取url和method
+$url=explode("?",$_SERVER["REQUEST_URI"],2);
+$method=$_SERVER["REQUEST_METHOD"];
 try {
-    $data=\Core\Router::runMatch($url,$method);
+    $data=\Core\Router::runMatch($url[0],$method);
 } catch (Throwable $throwable) {
     http_response_code(500);
     echo $throwable->getMessage();
